d647cdd0cfd9e6d4a60d370b773648fb67599e64
Orbasker <orbasker@gmail.com>
2023-05-10 21:26:54 +0300

    fixed index.html container

--- components/add-patient-form.js
+++ components/add-patient-form.js
@@ -4,7 +4,7 @@ class AddPatientForm extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = `<form class="p-4 sm:ml-64 m-16">
+        this.innerHTML = `<form id="form" class="p-4 sm:ml-64 m-16">
         <div class="space-y-12">
                 <div>
                 <h2 class="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
@@ -16,13 +16,13 @@ class AddPatientForm extends HTMLElement {
               <div class="sm:col-span-3">
                 <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">First name</label>
                 <div class="mt-2">
-                  <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                  <input type="text" placeholder=" first name" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                 </div>
               </div>
               <div class="sm:col-span-3 my-2">
                 <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Last name</label>
                 <div class="mt-2">
-                  <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                  <input type="text" placeholder=" last name" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                 </div>
               </div>
             </div>
@@ -31,7 +31,7 @@ class AddPatientForm extends HTMLElement {
                 <div class="col-span-3">
                     <label for="id" class="block text-sm font-medium leading-6 text-gray-900">ID</label>
                     <div class="mt-2">
-                      <input type="text" name="id" id="id" autocomplete="ID number" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                      <input type="text" placeholder=" Id number" name="id" id="id" autocomplete="ID number" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                     </div>
                   </div>
             </div>
@@ -42,69 +42,24 @@ class AddPatientForm extends HTMLElement {
               <div class="sm:col-span-3">
                 <label for="Department" class="block text-sm font-medium leading-6 text-gray-900">Department</label>
                 <div class="mt-2">
-                  <select id="department" name="department" autocomplete="department-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
-                    <option>Kids</option>
-                    <option>Emergency</option>
-                    <option>Babys</option>
-                    <option>Cardiology</option>
-                    <option>Orthopedics</option>
-                    <option>Urology</option>
-                    <option>General Surgery</option>
-                    <option>Plastic Surgery</option>
-                    <option>Obstetrics and Gynecology</option>
-                    <option>Psychiatry</option>
-                    <option>Ear, Nose and Throat</option>
-                    <option>Opthalmology</option>
-                    <option>Dermatology</option>
-                    <option>Endocrinology</option>
-                    <option>Gastroenterology</option>
-                    <option>Hematology</option>
-                    <option>Infectious Diseases</option>
-                    <option>Nephrology</option>
-                    <option>Pulmonology</option>
-                    <option>Rheumatology</option>
-                    <option>Neurology</option>
+                  <select id="departments" name="departments" autocomplete="department-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
+                    
                   </select>
                 </div>
               </div>
               <div class="sm:col-span-3">
                 <label for="Department" class="block text-sm font-medium leading-6 text-gray-900">Doctor</label>
                 <div class="mt-2">
-                  <select id="doctor" name="doctor" autocomplete="doctor-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
-                    <option>Dr Adam Levin</option>
-                    <option>Dr Barbara Cohen</option>
-                    <option>Dr Calvun Klein</option>
-                    <option>Dr David Cohen</option>
-                    <option>Dr Edward Cohen</option>
-                    <option>Dr Frank Cohen</option>
-                    <option>Dr George Cohen</option>
-                    <option>Dr Henry Cohen</option>
-                    <option>Dr Isaac Cohen</option>
+                  <select id="doctors" name="doctors" autocomplete="doctor-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
+                    
                   </select>
                 </div>
               </div>
               <div class="sm:col-span-3">
                 <label for="nurse" class="block text-sm font-medium leading-6 text-gray-900">Nurse</label>
                 <div class="mt-2">
-                  <select id="nurse" name="nurse" autocomplete="Nurse-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
-                    <option>Shifra levy</option>
-                    <option>Shira dotan</option>
-                    <option>Noa Cohen</option>
-                    <option>Yael Cohen</option>
-                    <option>Ashley Cooper</option>
-                    <option>Emma Cooper</option>
-                    <option>Olivia Cooper</option>
-                    <option>Isabella Cooper</option>
-                    <option>Ava Cooper</option>
-                    <option>Sophia Cooper</option>
-                    <option>Charlotte Cooper</option>
-                    <option>Mia Cooper</option>
-                    <option>Amelia Cooper</option>
-                    <option>Harper Cooper</option>
-                    <option>Evelyn Cooper</option>
-                    <option>Abigail Cooper</option>
-                    <option>Emily Cooper</option>
-                    <option>Ella Cooper</option>
+                  <select id="nurses" name="nurses" autocomplete="Nurse-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
+                  
                     
                   </select>
                 </div>
@@ -119,15 +74,15 @@ class AddPatientForm extends HTMLElement {
                 <p class="mt-1 text-sm leading-6 text-gray-600">These is estimated time to Realease.</p>
                 <div class="mt-6 space-y-6 m-20">
                   <div class="flex items-center gap-x-3">
-                    <input id="one_day" name="one_day" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
-                    <label for="one_day" class="block text-sm font-medium leading-6 text-gray-900">maximum 24 hours</label>
+                    <input id="one_day" name="one_day" type="radio" class="radio-button h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
+                    <label for="one_day" class=" block text-sm font-medium leading-6 text-gray-900">maximum 24 hours</label>
                   </div>
                   <div class="flex items-center gap-x-3">
-                    <input id="two_day" name="two_day" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
+                    <input id="two_day" name="two_day" type="radio" class="radio-button h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
                     <label for="two_day" class="block text-sm font-medium leading-6 text-gray-900">maximum 48 hours</label>
                   </div>
                   <div class="flex items-center gap-x-3">
-                    <input id="three_day" name="three_day" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
+                    <input id="three_day" name="three_day" type="radio" class= "radio-button h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
                     <label for="three_day" class="block text-sm font-medium leading-6 text-gray-900">maximum 72 hours</label>
                   </div>
                 </div>
---
+++ validate_form.js
@@ -0,0 +1,398 @@
+let hospitals = {
+  name: "Hospital XYZ",
+  departments: [
+    {
+      name: "Cardiology",
+      doctors: [
+        {
+          name: "Dr. Smith",
+          specialty: "Cardiologist",
+        },
+        {
+          name: "Dr. Johnson",
+          specialty: "Cardiologist",
+        },
+        {
+          name: "Dr. Lee",
+          specialty: "Cardiologist",
+        },
+      ],
+      nurse: [
+        {
+          name: "Sara Smith",
+          specialty: "Cardiology Nurse",
+        },
+        {
+          name: "Linda Cohen",
+          specialty: "Cardiology Nurse",
+        },
+        {
+          name: "Kim Wilson",
+          specialty: "Cardiology Nurse",
+        },
+      ],
+    },
+    {
+      name: "Orthopedics",
+      doctors: [
+        {
+          name: "Dr. Davis",
+          specialty: "Orthopedic Surgeon",
+        },
+        {
+          name: "Dr. Brown",
+          specialty: "Orthopedic Surgeon",
+        },
+      ],
+      nurse: [
+        {
+          name: "Sara Smith",
+          specialty: "Orthopedic Nurse",
+        },
+        {
+          name: "Linda Cohen",
+          specialty: "Orthopedic Nurse",
+        },
+        {
+          name: "Kim Wilson",
+          specialty: "Orthopedic Nurse",
+        },
+      ],
+    },
+    {
+      name: "Gastroenterology",
+      doctors: [
+        {
+          name: "Dr. Patel",
+          specialty: "Gastroenterologist",
+        },
+        {
+          name: "Dr. Kim",
+          specialty: "Gastroenterologist",
+        },
+      ],
+      nurse: [
+        {
+          name: "Sara Smith",
+          specialty: "Gastroenterologist",
+        },
+        {
+          name: "Dr. Kim",
+          specialty: "Gastroenterologist",
+        },
+        {
+          name: "Dr. Noya",
+          specialty: "Gastroenterologist",
+        },
+      ],
+    },
+    {
+      name: "Neurology",
+      doctors: [
+        {
+          name: "Dr. Lee",
+          specialty: "Neurologist",
+        },
+        {
+          name: "Dr. Kim",
+          specialty: "Neurologist",
+        },
+      ],
+      nurse: [
+        {
+          name: "Sara Smith",
+          specialty: "Neurology Nurse",
+        },
+        {
+          name: "Linda Cohen",
+          specialty: "Neurology Nurse",
+        },
+        {
+          name: "Kim Wilson",
+          specialty: "Neurology Nurse",
+        },
+        {
+          name: "Kim Wilson",
+          specialty: "Neurology Nurse",
+        },
+      ],
+    },
+    {
+      name: "Oncology",
+      doctors: [
+        {
+          name: "Dr. Lee",
+          specialty: "Oncologist",
+        },
+        {
+          name: "Dr. Kim",
+          specialty: "Oncologist",
+        },
+      ],
+      nurse: [
+        {
+          name: "Sara Smith",
+          specialty: "Oncology Nurse",
+        },
+        {
+          name: "Linda Cohen",
+          specialty: "Oncology Nurse",
+        },
+        {
+          name: "Kim Wilson",
+          specialty: "Oncology Nurse",
+        },
+      ],
+    },
+    {
+      name: "Pediatrics",
+      doctors: [
+        {
+          name: "Dr. Lee",
+          specialty: "Pediatrician",
+        },
+        {
+          name: "Dr. Kim",
+          specialty: "Pediatrician",
+        },
+      ],
+      nurse: [
+        {
+          name: "Sara Smith",
+          specialty: "Pediatric Nurse",
+        },
+        {
+          name: "Linda Cohen",
+          specialty: "Pediatric Nurse",
+        },
+        {
+          name: "Kim Wilson",
+          specialty: "Pediatric Nurse",
+        },
+      ],
+    },
+    {
+      name: "Psychiatry",
+      doctors: [
+        {
+          name: "Dr. Lee",
+          specialty: "Psychiatrist",
+        },
+        {
+          name: "Dr. Kim",
+          specialty: "Psychiatrist",
+        },
+      ],
+      nurse: [
+        {
+          name: "Sara Smith",
+          specialty: "Psychiatry Nurse",
+        },
+        {
+          name: "Linda Cohen",
+          specialty: "Psychiatry Nurse",
+        },
+        {
+          name: "Kim Wilson",
+          specialty: "Psychiatry Nurse",
+        },
+      ],
+    },
+    {
+      name: "Radiology",
+      doctors: [
+        {
+          name: "Dr. Lee",
+          specialty: "Radiologist",
+        },
+        {
+          name: "Dr. Kim",
+          specialty: "Radiologist",
+        },
+      ],
+      nurse: [
+        {
+          name: "Sara Smith",
+          specialty: "Radiology Nurse",
+        },
+        {
+          name: "Linda Cohen",
+          specialty: "Radiology Nurse",
+        },
+        {
+          name: "Kim Wilson",
+          specialty: "Radiology Nurse",
+        },
+      ],
+    },
+    {
+      name: "Urology",
+      doctors: [
+        {
+          name: "Dr. Lee",
+          specialty: "Urologist",
+        },
+        {
+          name: "Dr. Kim",
+          specialty: "Urologist",
+        },
+      ],
+      nurse: [
+        {
+          name: "Sara Smith",
+          specialty: "Urology Nurse",
+        },
+        {
+          name: "Linda Cohen",
+          specialty: "Urology Nurse",
+        },
+        {
+          name: "Kim Wilson",
+          specialty: "Urology Nurse",
+        },
+      ],
+    },
+  ],
+};
+
+function loadDepartments(hositals) {
+  let departmentsElement = document.getElementById("departments");
+  for (let department of hospitals.departments) {
+    let optionElement = document.createElement("option");
+    optionElement.value = department.name;
+    optionElement.innerHTML = department.name;
+    departmentsElement.appendChild(optionElement);
+  }
+}
+function loadDoctors(hositals) {
+  let doctorsElement = document.getElementById("doctors");
+  let selectedDepartment = document.getElementById("departments").value;
+  doctorsElement.innerHTML = "";
+  for (let department of hospitals.departments) {
+    if (department.name == selectedDepartment) {
+      for (let doctor of department.doctors) {
+        let optionElement = document.createElement("option");
+        optionElement.value = doctor.name;
+        optionElement.innerHTML = doctor.name;
+        doctorsElement.appendChild(optionElement);
+      }
+    }
+  }
+}
+
+function loadNurses(hositals) {
+  let nursesElement = document.getElementById("nurses");
+  let selectedDepartment = document.getElementById("departments").value;
+  nursesElement.innerHTML = "";
+  for (let department of hospitals.departments) {
+    if (department.name == selectedDepartment) {
+      for (let nurse of department.nurse) {
+        let optionElement = document.createElement("option");
+        optionElement.value = nurse.name;
+        optionElement.innerHTML = nurse.name;
+        nursesElement.appendChild(optionElement);
+      }
+    }
+  }
+}
+window.onload = function () {
+  loadDepartments(hospitals);
+  loadDoctors(hospitals);
+  loadNurses(hospitals);
+  let depElement = document.getElementById("departments");
+  depElement.addEventListener("change", loadDoctors);
+  depElement.addEventListener("change", loadNurses);
+};
+
+function isDoctorExists(doctorName) {
+  for (let department of hositals.departments) {
+    for (let doctor of department.doctors) {
+      if (doctor.name == doctorName) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+let formElement = document.getElementById("form");
+
+formElement.onsubmit = function (event) {
+  if (isFormValid()) {
+    return true;
+  } else {
+    event.preventDefault();
+  }
+};
+
+let alertRaised = false;
+
+function isFormValid() {
+  if (alertRaised == true) {
+    return false;
+  }
+  let validations = {
+    id: {
+      pattern: /[0-9]{9,10}/,
+      message: "Please enter a valid phone number, valid phone: 9-10 digits",
+    },
+    name: {
+      pattern: /[a-zA-Z]{2,}/,
+      message: "Please enter a valid name, valid name: 2 or more characters",
+    },
+  };
+
+  let alertElement = document.getElementById("alert");
+  let errorMessageElement = document.getElementById("errorMessage");
+  let ulElement = document.createElement("ul");
+  ulElement.className = "list-disc space-y-1 pl-5";
+
+  errorMessageElement.appendChild(ulElement);
+  let shouldAlert = false;
+
+  for (let [id, validation] of Object.entries(validations)) {
+    let inputElement = document.getElementById(id);
+    if (inputElement) {
+      let inputValue = inputElement.value;
+      let isValid = validation.pattern.test(inputValue);
+      if (!isValid) {
+        let liElement = document.createElement("li");
+        liElement.innerHTML = validation.message;
+        ulElement.appendChild(liElement);
+        shouldAlert = true;
+      }
+    }
+  }
+
+  if (shouldAlert) {
+    alertElement.removeAttribute("hidden");
+    alertRaised = true;
+
+    setTimeout(() => {
+      alertElement.setAttribute("hidden", true);
+      errorMessageElement.innerHTML = "";
+      alertRaised = false;
+    }, 5000);
+    return false;
+  } else {
+    return true;
+  }
+}
+
+const radioButtons = document.getElementsByClassName("radio-button");
+Array.from(radioButtons).forEach((radioButton) => {
+  radioButton.addEventListener("click", (event) => {
+    console.log(event.target + " is checked");
+    // uncheck all other radio buttons
+    Array.from(radioButtons).forEach((otherRadioButton) => {
+      if (otherRadioButton !== event.target) {
+        otherRadioButton.checked = false;
+      }
+    });
+  });
+});
+
+// function handleAlertClose() {
+//   let alertElement = document.getElementById("alert");
+//   alertElement.setAttribute("hidden", true);
+// }
