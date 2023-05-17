650d9ee42a02993df5a6b965d74ab996eaea181c
Nave Maymon <nave.may@gmail.com>
2023-05-17 11:53:35 +0300

    list

--- components/navbar-menu.js
+++ components/navbar-menu.js
@@ -9,18 +9,7 @@ class NavbarMenu extends HTMLElement {
           <div class="px-3 py-3 lg:px-5 lg:pl-3">
             <div class="flex items-center justify-between">
               <div class="flex items-start relative">
-                  <!-- Dropdown menu -->
-                  <svg id="avatarButton" fill="#000000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
-                    width="25px" height="25px" viewBox="0 0 45.532 45.532"
-                    xml:space="preserve" class="w-25 h-25 z-50">
-                  <g>
-                    <path d="M22.766,0.001C10.194,0.001,0,10.193,0,22.766s10.193,22.765,22.766,22.765c12.574,0,22.766-10.192,22.766-22.765
-                      S35.34,0.001,22.766,0.001z M22.766,6.808c4.16,0,7.531,3.372,7.531,7.53c0,4.159-3.371,7.53-7.531,7.53
-                      c-4.158,0-7.529-3.371-7.529-7.53C15.237,10.18,18.608,6.808,22.766,6.808z M22.761,39.579c-4.149,0-7.949-1.511-10.88-4.012
-                      c-0.714-0.609-1.126-1.502-1.126-2.439c0-4.217,3.413-7.592,7.631-7.592h8.762c4.219,0,7.619,3.375,7.619,7.592
-                      c0,0.938-0.41,1.829-1.125,2.438C30.712,38.068,26.911,39.579,22.761,39.579z"/>
-                  </g>
-                  </svg>
+                  <img src="../svg/peled.jpg" class="h-8" style="width: 50px; height: 50px; margin-left: 15px; border-radius: 50%;" alt="propeleduser">
                   <div id="userDropdown" class="hidden m-6 z-10 absolute bg-white divide-y divide-gray-100 shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                     <div class="px-4 py-3 text-sm text-gray-900 dark:text-white">
                       <div>Bonnie Green</div>
