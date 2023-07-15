c3cf4a4f2462bba960f14b0003256ee18cd4de73
Orbasker <orbasker@gmail.com>
2023-07-15 12:44:12 +0300

    navem working:

--- php/db.php
+++ php/db.php
@@ -5,46 +5,107 @@ include 'config.php';
 $connection = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
 
 if(mysqli_connect_errno()){
-    die("Connection failed: " . mysqli_connect_error());
+    $response = array(
+        'status' => 'error',
+        'message' => 'Connection failed',
+        'error' => mysqli_connect_error()
+    );
+    echo json_encode($response);
+    // die("Connection failed: " . mysqli_connect_error());
 }
-// print("Connection successful");
-$message = "Connection successful";
+// $message = "Connection successful";
+
+// function execute_query($query) {
+//     global $connection;
+//     try {
+//         $result = mysqli_query($connection, $query);
+//     } catch (mysqli_sql_exception $e) {
+//         $response = array(
+//             'status' => 'error',
+//             'message' => 'Query failed',
+//             'error' => mysqli_error($connection),
+//             'query' => $query
+//         );
+//         return $response;
+//     }
+
+//     if (strpos($query, 'SELECT') !== false) {
+//         $emparray = array();
+//         if (mysqli_num_rows($result) > 0) {
+//             while ($row = mysqli_fetch_assoc($result)) {
+//                 $emparray[] = $row;
+//             }
+//             $response = json_encode($emparray);
+//             return $response;
+//         } else {
+//             $response = array(
+//                 'status' => 'error',
+//                 'message' => 'Query failed',
+//                 'error' => mysqli_error($connection),
+//                 'query' => $query
+//             );
+//             return $response;
+//         }
+//     } else if (strpos($query, 'INSERT') !== false || strpos($query, 'UPDATE') !== false || strpos($query, 'DELETE') !== false) {
+//         return $result; // Return the result for INSERT queries
+//     } else {
+//         $response = array(
+//             'status' => 'error',
+//             'message' => 'Query failed',
+//             'error' => mysqli_error($connection),
+//             'query' => $query
+//         );
+//         // Return true for non-SELECT and non-INSERT queries
+//         return $response;
+//     }
+// }
+
 
 function execute_query($query) {
     global $connection;
-    $result = mysqli_query($connection, $query);
-    if (!$result) {
-        echo $query;
-        echo "Problem with the query";
-        die("Query Failed: " . mysqli_error($connection));
+    try {
+        $result = mysqli_query($connection, $query);
+    } catch (mysqli_sql_exception $e) {
+        $response = array(
+            'status' => 'error',
+            'message' => 'Query failed',
+            'error' => $e->getMessage(),
+            'query' => $query
+        );
+        return json_encode($response);
     }
-    
+
     if (strpos($query, 'SELECT') !== false) {
         $emparray = array();
         if (mysqli_num_rows($result) > 0) {
             while ($row = mysqli_fetch_assoc($result)) {
                 $emparray[] = $row;
             }
-            $result = json_encode($emparray);
-            return $result;
+            $response = array(
+                'status' => 'success',
+                'data' => $emparray
+            );
+            return json_encode($response);
         } else {
-            return null;
+            $response = array(
+                'status' => 'error',
+                'message' => 'No data found',
+                'query' => $query
+            );
+            return json_encode($response);
         }
     } else if (strpos($query, 'INSERT') !== false || strpos($query, 'UPDATE') !== false || strpos($query, 'DELETE') !== false) {
-        // echo $query;
-        // echo $result;
-        // $result = mysqli_insert_id($connection);
-        return $result; // Return the result for INSERT queries
+        $response = array(
+            'status' => 'success',
+            'message' => 'Query successful'
+        );
+        return json_encode($response);
     } else {
-        echo $query;
-        // Return true for non-SELECT and non-INSERT queries
-        return true;
+        $response = array(
+            'status' => 'error',
+            'message' => 'Invalid query type',
+            'query' => $query
+        );
+        return json_encode($response);
     }
 }
-
-// if (isset($_POST['logout']) || isset($_SESSION)) {
-//     session_destroy();
-// }
-// mysqli_close($connection);
-?>
-
--- php/login.php
+++ php/login.php
@@ -1,6 +1,5 @@
 <?php
 include 'db.php';
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // var_dump($_POST);
     // $request_data = file_get_contents('php://input');
@@ -11,13 +10,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             'message' => 'Please fill in the required fields'
         );
     } else {
-        $get_user_query = "SELECT * FROM dbShnkr23stud2.tbl_209_users WHERE username = ".$_POST['user_name']." AND user_password = ".$_POST['password']."";
+        $get_user_query = "SELECT * FROM dbShnkr23stud2.tbl_209_users WHERE username = ".'"'.$_POST['user_name'].'"'." AND user_password = '".$_POST['password']."'";
         // var_dump($get_user_query);
         $result = execute_query($get_user_query);
+        // $result = json_encode($result);
+        $result = json_decode($result, true);
+        // $result = json_decode($result);
         // var_dump($result);
-        if ($result) {
-            $row = json_decode($result, true);
-            $result = $row[0];
+        if (isset($result['status']) && $result['status'] === 'success' ) {
+            if (is_array($result['data'])) {
+                // $row = json_decode($result['data'], true);
+                // $result = $row[0];
+                $result = $result['data'][0];
+            }
+            // $result_string = json_encode($result);
             session_start();
             $_SESSION["user_id"] = $result['user_id'];
             $_SESSION["user_name"] = $result['username'];
@@ -33,7 +39,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         } else {
             $response = array(
                 'status' => 'inactive',
-                'message' => 'Invalid Username or Password!'
+                'message' => 'Invalid Username or Password!',
+                'result' => $result,
+                'query' => $get_user_query
             );
         }
     }
@@ -46,3 +54,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 echo json_encode($response);
 ?>
+
