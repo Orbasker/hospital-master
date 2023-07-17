1d9af499a791a0aaa85367c230d0be350dc7ffc0
Nave Maymon <nave.may@gmail.com>
2023-07-17 11:27:54 +0300

    login  fixed - nave

--- login.php
+++ login.php
@@ -9,9 +9,7 @@
 <body>
     <section class="bg-gray-50 ">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900">
-                <img class="w-18 h-12 mr-6" src="assets/svg/logo1.svg" alt="logo">
-            </a>
+            <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900"><img class="w-18 h-12 mr-6" src="assets/svg/logo1.svg" alt="logo"></a>
             <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                     <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl text-center">Sign in to your account</h1>
