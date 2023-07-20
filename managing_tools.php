11081142916cd53248da5fa5b504ed5903a87116
Nave Maymon <nave.may@gmail.com>
2023-07-20 13:55:44 +0300

    favicon

--- managing_tools.php
+++ managing_tools.php
@@ -2,28 +2,24 @@
 include 'http://se.shenkar.ac.il/students/2022-2023/web1/dev_209/php/sessions_managment.php?action=check_session';
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
   <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.15/dist/tailwind.min.css"
-      rel="stylesheet"
-    />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.15/dist/tailwind.min.css" rel="stylesheet">
     <title>Managing Tools - user administration</title>
+    <link rel="icon" type="image/png" href="assets/svg/traffic-light-16.png">
     <script src="http://se.shenkar.ac.il/students/2022-2023/web1/dev_209/js/components/navbar-menu.js"></script>
     <script src="http://se.shenkar.ac.il/students/2022-2023/web1/dev_209/js/components/managing-tool.js"></script>
   </head>
   <body>
     <navbar-menu>
       <div class="container sm:ml-64">
-
       <managing-tool></managing-tool>
       </div>
-    </navbar-menu>
-          <script src="http://se.shenkar.ac.il/students/2022-2023/web1/dev_209/js/managing_tools.js"></script>
+      </navbar-menu>
+        <script src="http://se.shenkar.ac.il/students/2022-2023/web1/dev_209/js/managing_tools.js"></script>
         <script src="http://se.shenkar.ac.il/students/2022-2023/web1/dev_209/js/validate_user.js"></script>
-
   </body>
 </html>
 
