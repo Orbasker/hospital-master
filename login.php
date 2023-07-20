cab113ff06a8850f54c7c99dd6c9758454e334cb
Nave Maymon <nave.may@gmail.com>
2023-07-20 13:27:15 +0300

    index

--- login.php
+++ login.php
@@ -2,10 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>login - Hospital Master</title>
+    <title>LogIn - Hospital Master</title>
     <link rel="icon" type="image/png" href="assets/svg/favicon-16x16.png">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/png" href="assets/svg/traffic-light-16.png">
 </head>
 <body>
     <section class="bg-gray-50 ">
@@ -16,11 +17,11 @@
                     <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl text-center">Sign in to your account</h1>
                     <form class="space-y-4 md:space-y-6" action="#" method="post" id="login_form">
                         <div>
-                            <label for="user_name" class="block mb-2 text-sm font-medium text-gray-900">Enter Username</label>
+                            <label for="user_name" class="block mb-2 text-sm font-medium text-gray-900">Enter Username:</label>
                             <input type="text" name="user_name" id="user_name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="username" pattern=".{3,}" required>
                         </div>
                         <div>
-                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Enter Password:</label>
                             <input type="password" name="password" id="password" placeholder="Four or more characters" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" pattern=".{4,}" required>
                         </div>
                         <div class="flex items-center justify-between">
@@ -32,7 +33,6 @@
                         <p class="text-sm font-light text-blak">Don’t have an account yet?<a href="https://www.hadassah.org.il/contact-us/" target="_blank" class="font-medium text-primary-600 hover:underline">Sign Up</a></p>
                     </form>
                     <message-modal></message-modal>
-
                 </div>
             </div>
         </div>
